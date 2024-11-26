f78de0891356d5bf062b7ab175bfc9679b2bd0dc
Srilatha Dama <srilathadama9@gmail.com>
2024-11-25 19:47:40 -0800

    Time: 1930 ms (30.03%), Space: 29.1 MB (59.76%) - LeetHub

---
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
+        rows, cols = len(box), len(box[0])
+        ans = list(box)
+
+        for i, row in enumerate(box):
+            start = 0
+            for j, value in enumerate(row):
+                if value != '#':
+                    start = j + 1
+                else:
+                    if j + 1 < cols and box[i][j + 1] == '.':
+                        ans[i][start], ans[i][j + 1] = '.', '#'
+                        start += 1
+
+        return [[ans[i][j] for i in range(rows - 1, -1, -1)] for j in range(cols)]
